a9e1caacc03f05b947e650bd0626df517807ddff
Safidy3 <safidy.anyaina@gmail.com>
2025-01-11 12:01:46 +0300

    ADD M1/ex05

--- Module_1/ex05/Harl.cpp
+++ Module_1/ex05/Harl.cpp
@@ -1,12 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*    Harl.cpp                                          :+:      :+:    :+:   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: safandri <safandri@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 10:24:21 by safandri          #+#    #+#             */
-/*   Updated: 2025/01/09 10:24:21 by safandri         ###   ########.fr       */
+/*   Updated: 2025/01/11 10:14:27 by safandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Harl.hpp"
+
+void    Harl::debug()
+{
+	std::cout << "DEBUG level\n";
+}
+
+void    Harl::info()
+{
+	std::cout << "INFO level\n";
+}
+
+void    Harl::warning()
+{
+	std::cout << "WARNING level\n";
+}
+
+void    Harl::error()
+{
+	std::cout << "ERROR level\n";
+}
+
+void    Harl::complain(std::string level)
+{
+	debug();
+}
