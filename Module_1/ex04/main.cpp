4027d5f18ecd416f47eb0ffd5cc9ce08e45394cc
Safidy3 <safidy.anyaina@gmail.com>
2025-01-15 11:29:23 +0300

    fix : ex04

--- Module_1/ex04/main.cpp
+++ Module_1/ex04/main.cpp
@@ -6,29 +6,76 @@
 /*   By: safandri <safandri@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 10:24:19 by safandri          #+#    #+#             */
-/*   Updated: 2025/01/09 10:24:19 by safandri         ###   ########.fr       */
+/*   Updated: 2025/01/15 11:28:34 by safandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include <iostream>
 #include <fstream>
 #include <string>
 
-void replace(std::string& str, const std::string& to_replace, const std::string& replacement) {
-    std::size_t pos = str.find(to_replace, pos);
-    while (pos != std::string::npos) {
-        str.erase(pos, to_replace.length());
-        str.insert(pos, replacement);
-        pos += replacement.length();
-    }
+std::string	replacedText(std::ifstream &inputFile, std::string s1, std::string s2)
+{
+	std::string line;
+	std::string strResult;
+	std::size_t pos;
+
+    if (std::getline(inputFile, line, '\0'))
+	{
+   		pos = line.find(s1, 0);
+		while (pos != std::string::npos)
+		{
+			line.erase(pos, s1.length());
+			line.insert(pos, s2);
+			pos += s2.length();
+			pos = line.find(s1, pos);
+		}
+		strResult.append(line);
+	}
+	return (strResult);
 }
 
-int main ()
+void	init_vars(std::string &filename, std::string &s1, std::string &s2, char *argv[])
 {
-	std::string str = "There are two needles in this haystack with needles.";
-	std::string str2 = "needles";
+	std::string	tmp;
+	std::size_t	found;
 
-  	std::cout << str << '\n';
-	replace(str, str2, "universal");
-  	std::cout << str << '\n';
+	s1.assign(argv[2]);
+	s2.assign(argv[3]);
+	tmp.assign(argv[1]);
+
+	found = tmp.find(".");
+	if (found != std::string::npos)
+		filename.assign(tmp, 0, tmp.find("."));
+	else
+		filename.assign(tmp);
+	filename.append(".replace");
+}
+
+int main(int argc, char *argv[])
+{
+	std::ifstream	inputFile;
+	std::ofstream	outputFile;
+	std::string		outputFileName;
+	std::string		s1;
+	std::string		s2;
+	std::string		strResult;
+
+	if (argc != 4)
+	{
+		std::cout << "Error : invalid argument\n";
+		return (1);
+	}
+	inputFile.open(argv[1], std::ios::in);
+	if (!inputFile.is_open())
+	{
+		std::cout << "Error : there was a problem oppening the file\n";
+		return (1);
+	}
+	init_vars(outputFileName, s1, s2, argv);
+	strResult = replacedText(inputFile, s1, s2);
+	outputFile.open(outputFileName.c_str(), std::ios::out);
+	outputFile << strResult;
+	inputFile.close();
 	return (0);
 }
