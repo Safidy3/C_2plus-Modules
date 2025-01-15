01af909bdf97b5bb81b0da73c1d5eef6b168d747
Safidy3 <safidy.anyaina@gmail.com>
2025-01-15 11:46:09 +0300

    Fix : ex06

--- Module_1/ex06/Harl.cpp
+++ Module_1/ex06/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: safandri <safandri@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 10:24:21 by safandri          #+#    #+#             */
-/*   Updated: 2025/01/11 10:14:27 by safandri         ###   ########.fr       */
+/*   Updated: 2025/01/15 11:45:46 by safandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,15 +34,31 @@ void	Harl::error()
 
 void	Harl::complain(std::string level)
 {
-	std::map<std::string, void (Harl::*)(void)> actions;
-    actions["DEBUG"] = &Harl::debug;
-    actions["INFO"] = &Harl::info;
-    actions["WARNING"] = &Harl::warning;
-    actions["ERROR"] = &Harl::error;
+	std::map<std::string, int> actions;
+    actions["DEBUG"] = 0;
+    actions["INFO"] = 1;
+    actions["WARNING"] = 2;
+    actions["ERROR"] = 3;
 
-	std::map<std::string, void (Harl::*)(void)>::iterator it = actions.find(level);
-	if (it != actions.end())
-		this->Harl::debug();
-	else
-		std::cout << "[ Probably complaining about insignificant problems ]";
+	std::map<std::string, int>::iterator it = actions.find(level);
+	if (it == actions.end())
+	{
+		std::cout << "[ Probably complaining about insignificant problems ]\n";
+		return ;
+	}
+	switch (it->second)
+	{
+		case 0:
+			this->Harl::debug();
+			//fall through
+		case 1:
+			this->Harl::info();
+			//fall through
+		case 2:
+			this->Harl::warning();
+			//fall through
+		case 3:
+			this->Harl::error();
+			break;
+	}
 }
--- Module_1/ex06/main.cpp
+++ Module_1/ex06/main.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: safandri <safandri@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/09 10:24:21 by safandri          #+#    #+#             */
-/*   Updated: 2025/01/11 10:14:27 by safandri         ###   ########.fr       */
+/*   Updated: 2025/01/15 11:44:58 by safandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,17 @@
 
 int	main(int argc, char *argv[])
 {
+	(void)argc;
 	Harl	harl;
+	std::string	level;
 
-	harl.complain(argv[1]);
+	if (argc != 2)
+	{
+		std::cout << "Error : invalid argument number\n";
+		return (1);
+	}
+
+	level.assign(argv[1]);
+	harl.complain(level);
 	return (0);
 }
