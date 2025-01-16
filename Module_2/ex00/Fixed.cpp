32a9f6dc6c5ef279a534d2d2f17425343356d9af
safidy3 <safidy.anyaina@gmail.com>
2025-01-16 06:53:39 +0300

    add m2/ex00

--- Module_2/ex00/Fixed.cpp
+++ Module_2/ex00/Fixed.cpp
@@ -10,3 +10,44 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.hpp"
+
+static const int	floating_point = 8;
+
+Fixed::Fixed()
+{
+	std::cout << "Default constructor called\n";
+}
+
+Fixed::Fixed(Fixed const &fixed)
+{
+	this->fixed_point = fixed.fixed_point;
+	std::cout << "Copy constructor called\n";
+}
+
+Fixed& Fixed::operator=(Fixed const &fixed)
+{
+	std::cout << "Copy assignment operator called\n";
+	if (this == &fixed)
+		return (*this);
+	this->setRawBits(fixed.fixed_point);
+	return (*this);
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called\n";
+}
+
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called\n";
+	return (this->fixed_point);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	this->fixed_point = raw;
+	std::cout << "setRawBits member function called\n";
+}
+
--- Module_2/ex00/Fixed.hpp
+++ Module_2/ex00/Fixed.hpp
@@ -10,3 +10,23 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class	Fixed
+{
+	private:
+		int					fixed_point;
+		static const int	floating_point;
+	public:
+		Fixed();
+		~Fixed();
+		Fixed(Fixed const &obj);
+		Fixed& operator=(const Fixed &obj);
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+};
+
+#endif
