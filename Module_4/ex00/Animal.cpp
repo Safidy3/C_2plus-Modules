592d0911f7edacae249927811362c2d88212137d
Safidy3 <safidy.anyaina@gmail.com>
2025-01-28 15:46:55 +0300

    add : M4/ex00 files

--- Module_4/ex00/Animal.cpp
+++ Module_4/ex00/Animal.cpp
@@ -1,29 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Animal.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: safandri <safandri@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/28 15:44:17 by safandri          #+#    #+#             */
-/*   Updated: 2025/01/28 15:45:56 by safandri         ###   ########.fr       */
+/*   Created: 2025/01/28 15:44:28 by safandri          #+#    #+#             */
+/*   Updated: 2025/01/28 15:44:29 by safandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Animal.hpp"
-
-int main()
-{
-	const Animal* meta = new Animal();
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
-
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
-
-	i->makeSound();
-	j->makeSound();
-	meta->makeSound();
-
-	return (0);
-}
--- Module_4/ex00/Animal.hpp
+++ Module_4/ex00/Animal.hpp
@@ -1,36 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*   Animal.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: safandri <safandri@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/18 13:35:05 by safandri          #+#    #+#             */
-/*   Updated: 2025/01/19 10:30:26 by safandri         ###   ########.fr       */
+/*   Created: 2025/01/28 15:44:31 by safandri          #+#    #+#             */
+/*   Updated: 2025/01/28 15:44:53 by safandri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CLAPTRAP_HPP
-# define CLAPTRAP_HPP
-# include <iostream>
-
-class ClapTrap
-{
-	private:
-		std::string	name;
-		int			hit_point;
-		int			energy;
-		int			attackDamage;
-	public:
-		ClapTrap();
-		ClapTrap(std::string name_);
-		~ClapTrap();
-
-		void	showStats();
-
-		void	Attack(const std::string& target);
-		void	TakeDamage(unsigned int amount);
-		void	BeRepaired(unsigned int amount);
-};
-
-#endif
